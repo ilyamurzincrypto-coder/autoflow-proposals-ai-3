8e4766223e7aa3c088b4fbae37b4764b96c70f59
ilyamurzincrypto-coder <ilya.murzin.crypto@gmail.com>
2026-03-25 21:43:43 +0600

    Delete src directory

--- src/utils/format.js
+++
@@ -1,8 +0,0 @@
-export const fmt = (n) => new Intl.NumberFormat("ru-RU").format(n) + " ₽";
-
-export const fmtDate = (d) =>
-  new Date(d).toLocaleDateString("ru-RU", {
-    day: "numeric",
-    month: "short",
-    year: "numeric",
-  });
